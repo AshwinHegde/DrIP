3b3a5ea8698eb7fef53f3c86b8467dcc27bf3938
AshwinHegde <ubuntu@ip-172-31-21-34.us-west-2.compute.internal>
2019-09-28 19:22:33 +0000

    Minor edits and improved documentation

--- Drug_Interaction_Predictor/read_data.py
+++ Drug_Interaction_Predictor/read_data.py
@@ -4,46 +4,32 @@ import lxml.etree as et
 import xml.etree.ElementTree as ET
 
 
-# TODO : Paths are included as arguments to read functions.
-# Maybe include this in an external file that user can modify
-# TODO : Improve documentation.
-
-
 # Contains functions to read data from XML file obtained from DrugBank.
 # Separate functions to read locally and to read from S3 storage.
 #
-# Defines a Drug class that for my purposes has name, structure
-# and interactions as attributes. The XML file from DrugBank has
-# much more information than read here and if necessary these can
-# be added to the class definition. Note that then, extra commands
-# must be written into read_data to read these attributes.
+# Defines a Drug class that has name, structure
+# and interactions as attributes.
 # 
-# Read functions return a list of Drug objects and a dictionary
-# of drug names with corresponding SMILES representations.
-
 # Also defines an Interaction class that contains the names of
-# two drugs, their SMILES representation and a textual description
+# two drugs and a textual description
 # of their interaction.
 
-# A function (get_interaction) returns a list of all interactions
-# (Interaction objects) present in a list of drugs (Drug objects).
-# Drugs without structural information are filtered out.
-
-
-
 class Drug:
+    '''Contains relevant features of each drug
+
+    Attributes :
+        name (str): Name of drug
+        structure (str): SMILES representation of drug
+        interactions (list): List of 
     '''
-    Contains relevant features of each drug : Name, SMILES representations and all the drugs it interacts with
-    '''
-    def __init__(self):
-        self.name = None
-        self.structure = None
-        self.interactions = None
+    def __init__(self, name, structure, interactions):
+        self.name = name
+        self.structure = structure
+        self.interactions = interactions
 
     def __repr__(self):
         return 'Name : {}\nStructure : {}'.format(self.name, self.structure)
     
-    # Method to check if drug has structural data
     def has_structure(self):
         if self.structure is not None:
             return True
@@ -52,31 +38,38 @@ class Drug:
 
 
 class Interaction:
+    '''Contains a drug pair and a textual description of the interaction.
+
+    Attributes :
+        druga (str): Name of first drug
+        drugb (str): Name of second drug
+        description (str): Text about the effect of taking the drug together
     '''
-    Contains names and SMILES representations of a pair of drugs along with a textual description of the kind of interaction.
-    '''
-    def __init__(self, druga = None, smilesa = None, drugb = None, smilesb = None, description = None):
+
+    def __init__(self, druga = None, drugb = None, description = None):
         self.druga = druga
-        self.smilesa = smilesa
         self.drugb = drugb
-        self.smilesb = smilesb
         self.description = description
     
-    def __repr__(self):
-        return 'Drug A : {}\nSMILES : {}\nDrug B : {}\nSMILES : {}\nInteraction description : {}'.format(self.druga, self.smilesa, self.drugb, self.smilesb, self.description)
-    
-    def assign(self, druga, smilesa, drugb, smilesb, description):
+    def assign(self, druga, drugb, description):
         self.druga = druga
         self.drugb = drugb
-        self.smilesa = smilesa
-        self.smilesb = smilesb
         self.description = description
 
 
-def read_from_bucket(bucket = 'insight-ashwin-s3-bucket', key = 'drugbank_data/drugbank_data.xml', number_of_drugs = 50000):
+def read_from_bucket(bucket = 'insight-ashwin-s3-bucket', \
+    key = 'drugbank_data/drugbank_data.xml', number_of_drugs = 50000):
+    '''Function to read an XML file from AWS S3 storage
+
+    Args : 
+        bucket (str): Name of bucket on S3
+        key (str): Location of XML file in the bucket
+        number_of_drugs (int): Total number of drugs to be read
+
+    Returns :
+        drug_list (list): List of Drug instances initialized with data read.
     '''
-    Function to read an XML file from S3 and return a list of Drug objects. It takes as input number of drugs to read, the name of the bucket on S3 and location(key) of the file in the bucket. Invokes read_data function.
-    '''
+
     s3 = boto3.resource('s3', use_ssl = False, verify = False)
     obj = s3.Object(bucket, key)
     file = obj.get()['Body'].read()
@@ -85,41 +78,59 @@ def read_from_bucket(bucket = 'insight-ashwin-s3-bucket', key = 'drugbank_data/d
 
 
 def read_from_file(xml_file, number_of_drugs = 50000):
-    '''
-    Function to read an XML file locally and return a list of Drug objects. It takes as input the XML file location and number of drugs to be read. Invokes read_data function.
+    '''Function to read an XML file locally
+
+    Args :
+        xml_file (str): Name of XML file
+        number_of_drugs (int): Total number of drugs to be read
+
+    Returns :
+        drug_list (list): List of Drug instances initialized with data read.
     '''
     tree = ET.parse(xml_file)
     return read_data(tree, number_of_drugs)
 
 
 def read_data(tree, number_of_drugs = 50000, addon = '{http://www.drugbank.ca}'):
-    '''
-    Function that takes an ElementTree object as input and parses it to extract relevant features of a drug. Can enter number of drugs to be read and a string(addon) to be added on to all tags depending on XML file. The default addon corresponds to that of DrugBank data. Returns a list of Drug objects.
+    '''Function to read an XML file given the tree object.
+
+    Args :
+        tree (ElementTree): ElementTree instance of the XML file to be read
+        number_of_drugs (int): Total number of drugs to be read
+        addon (str): default text to be added to XML tags
+
+    Returns :
+        drug_list (list): List of Drug instances initialized with data read.
+        smiles_dict (dict): Dictionary that maps drug names to their SMILES strings.
     '''
     root = tree.getroot()
     drug_count = 0
     drug_list = []
     smiles_dict = {}
     for elem in root:
-        drug = Drug()
+        structure = ''
+        interactions = {}
+
         # go through the drug descriptions and extract relevant features of each drug : 
         for i in range(len(elem)):
             if elem[i].tag == addon+'name':
-                drug.name = (elem[i].text).lower()
+                name = (elem[i].text).lower()
                 
             if elem[i].tag == addon+'calculated-properties':
                 calculated_properties = elem[i]
                 for calc_property in calculated_properties:
                     if calc_property[0].text == 'SMILES':
-                        drug.structure = calc_property[1].text
+                        structure = calc_property[1].text
                         
             if elem[i].tag ==  addon+'drug-interactions':
-                interactions = elem[i]
-                drug.interactions = {inter[1].text.lower():inter[2].text for inter in interactions}
+                interactions_list = elem[i]
+                interactions = {inter[1].text.lower():inter[2].text.lower()\
+                     for inter in interactions_list}
 
-        if drug.has_structure():
+        if structure != '' and interactions != {}:
+            drug = Drug(name, structure, interactions)
             drug_list.append(drug)
-            smiles_dict[drug.name] = drug.structure
+            smiles_dict[name] = structure
             drug_count += 1
             if drug_count >= number_of_drugs:
                 break
@@ -128,8 +139,14 @@ def read_data(tree, number_of_drugs = 50000, addon = '{http://www.drugbank.ca}')
 
 
 def generate_interactions(drug_list, smiles_dict):
-    '''
-    Function that takes a list of Drug objects and returns interaction pairs (in terms of Interaction objects) along with their SMILES representation and a textual description of the interaction. A drug name to SMILES dictionary is passed using which drugs without SMILES data are filtered out. Returns a list of Interaction objects.
+    '''Function to extract interaction pairs from a list of drugs
+
+    Args :
+        drug_list (list): List of Drug instances initialized with data read.
+        smiles_dict (dict): Dictionary that maps drug names to their SMILES strings.
+
+    Returns :
+        interaction_list (list): List of Interaction instances extracted from drug_list.
     '''
     interaction_list = []
     interaction_count = 0
@@ -137,7 +154,7 @@ def generate_interactions(drug_list, smiles_dict):
         for drugb, description in drug.interactions.items():
             # Only keep drugs that have structural data
             if drugb in smiles_dict:
-                interaction = Interaction(drug.name, drug.structure, drugb, smiles_dict[drugb], description)
+                interaction = Interaction(drug.name, drugb, description)
                 interaction_list.append(interaction)
                 interaction_count += 1
 
